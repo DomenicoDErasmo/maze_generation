32d415133e6e31fd5ba3f8177ffbb33b0ab5b2bb
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2024-08-24 23:37:21 -0400

    slight refactor

--- src/maze.rs
+++ src/maze.rs
@@ -59,8 +59,8 @@ impl Maze {
         let mut board = Self::blank_board(height, width)?;
 
         let mut visited = Board::<VisitStatus>::new(height, width);
-        let start = choose_perimeter_pair(&board)?;
-        add_maze_entry(start, &mut board);
+        let start = Self::choose_perimeter_pair(&board)?;
+        Self::add_maze_entry(start, &mut board);
 
         let mut visited_stack: Stack<Pair> = Stack::new();
 
@@ -71,7 +71,7 @@ impl Maze {
             let popped_pair = visited_stack.top()?;
 
             let Some(direction) =
-                choose_random_unvisited_direction(popped_pair, &visited)
+                Self::choose_random_unvisited_direction(popped_pair, &visited)
             else {
                 visited_stack.pop();
                 continue;
@@ -87,8 +87,8 @@ impl Maze {
             *visited.get_mut_from_pair(in_between_pair)? = VisitStatus::Visited;
         }
 
-        let end = choose_perimeter_pair(&board)?;
-        add_maze_entry(end, &mut board);
+        let end = Self::choose_perimeter_pair(&board)?;
+        Self::add_maze_entry(end, &mut board);
 
         Some(Self { board })
     }
@@ -121,136 +121,136 @@ impl Maze {
 
         Some(grid)
     }
-}
 
-/// Adds an entry point to the maze.
-///
-/// ### Parameters
-/// * `pair`: The `Pair` adjacent to the perimeter of the board.
-/// * `board`: The board of `Tiles` to update.
-fn add_maze_entry(perimeter_tile: Perimeter, board: &mut Board<Tile>) {
-    for direction in Direction::iter() {
-        let Some(_) = board.get_from_pair(
-            perimeter_tile.pair.add(2_i32.mul(Pair::from(direction))),
-        ) else {
-            let Some(cell) = board.get_mut_from_pair(
-                perimeter_tile.pair.add(Pair::from(direction)),
+    /// Adds an entry point to the maze.
+    ///
+    /// ### Parameters
+    /// * `pair`: The `Pair` adjacent to the perimeter of the board.
+    /// * `board`: The board of `Tiles` to update.
+    fn add_maze_entry(perimeter_tile: Perimeter, board: &mut Board<Tile>) {
+        for direction in Direction::iter() {
+            let Some(_) = board.get_from_pair(
+                perimeter_tile.pair.add(2_i32.mul(Pair::from(direction))),
             ) else {
+                let Some(cell) = board.get_mut_from_pair(
+                    perimeter_tile.pair.add(Pair::from(direction)),
+                ) else {
+                    return;
+                };
+
+                *cell = Tile::Entry;
                 return;
             };
-
-            *cell = Tile::Entry;
-            return;
-        };
-    }
-}
-
-/// Chooses a `Pair` from the perimeter of the maze.
-///
-/// ### Parameters
-/// * `board`: A reference to the board to get a perimeter cell from.
-///
-/// ### Returns
-/// * An optional pair.
-fn choose_perimeter_pair(board: &Board<Tile>) -> Option<Perimeter> {
-    let side = Direction::iter()
-        .collect::<Vec<Direction>>()
-        .choose(&mut thread_rng())
-        .copied()
-        .unwrap_or_default();
-
-    println!("{side:#?}");
-
-    match side {
-        Direction::Down => {
-            let row = i32::try_from(board.grid.len().sub(2)).ok()?;
-            let col = i32::try_from(Board::<Tile>::cell_position_to_index(
-                thread_rng().gen_range(0..board.cell_width),
-            ))
-            .ok()?;
-            Some(Perimeter {
-                pair: Pair { row, col },
-            })
-        }
-        Direction::Left => {
-            let row = i32::try_from(Board::<Tile>::cell_position_to_index(
-                thread_rng().gen_range(0..board.cell_width),
-            ))
-            .ok()?;
-            let col = 1;
-            Some(Perimeter {
-                pair: Pair { row, col },
-            })
-        }
-        Direction::Up => {
-            let row = 1;
-            let col = i32::try_from(Board::<Tile>::cell_position_to_index(
-                thread_rng().gen_range(0..board.cell_width),
-            ))
-            .ok()?;
-            Some(Perimeter {
-                pair: Pair { row, col },
-            })
-        }
-        Direction::Right => {
-            let row = i32::try_from(board.grid.len().sub(2)).ok()?;
-            let col = i32::try_from(Board::<Tile>::cell_position_to_index(
-                thread_rng().gen_range(0..board.cell_width),
-            ))
-            .ok()?;
-            Some(Perimeter {
-                pair: Pair { row, col },
-            })
         }
     }
-}
 
-/// Choosese a random unvisited direction.
-///
-/// ### Parameters
-/// * `pair`: A `Pair` to access a `Board` with.
-/// * `visited`: The `Board` of visitation status.
-///
-/// ### Returns
-/// * An optional direction.
-#[inline]
-#[must_use]
-pub fn choose_random_unvisited_direction(
-    pair: Pair,
-    visited: &Board<VisitStatus>,
-) -> Option<Direction> {
-    let direction_choices = get_unvisited_directions(pair, visited);
-    direction_choices
-        .into_iter()
-        .collect::<Vec<Direction>>()
-        .choose(&mut thread_rng())
-        .copied()
-}
+    /// Chooses a `Pair` from the perimeter of the maze.
+    ///
+    /// ### Parameters
+    /// * `board`: A reference to the board to get a perimeter cell from.
+    ///
+    /// ### Returns
+    /// * An optional pair.
+    fn choose_perimeter_pair(board: &Board<Tile>) -> Option<Perimeter> {
+        let side = Direction::iter()
+            .collect::<Vec<Direction>>()
+            .choose(&mut thread_rng())
+            .copied()
+            .unwrap_or_default();
 
-/// Gets unvisited directions.
-///
-/// ### Parameters
-/// * `pair`: A `Pair` to access a `Board` with.
-/// * `visited`: The `Board` of visitation status.
-///
-/// ### Returns
-/// * A set of `Direction` enums.
-#[inline]
-#[must_use]
-pub fn get_unvisited_directions(
-    pair: Pair,
-    visited: &Board<VisitStatus>,
-) -> HashSet<Direction> {
-    Direction::iter()
-        .filter(|direction| {
-            let Some(visit_status_of_new_pair) = visited
-                .get_from_pair(pair.add(2_i32.mul(Pair::from(*direction))))
-            else {
-                return false;
-            };
-            *visit_status_of_new_pair == VisitStatus::Unvisited
-        })
-        .collect::<HashSet<Direction>>()
+        println!("{side:#?}");
+
+        match side {
+            Direction::Down => {
+                let row = i32::try_from(board.grid.len().sub(2)).ok()?;
+                let col = i32::try_from(Board::<Tile>::cell_position_to_index(
+                    thread_rng().gen_range(0..board.cell_width),
+                ))
+                .ok()?;
+                Some(Perimeter {
+                    pair: Pair { row, col },
+                })
+            }
+            Direction::Left => {
+                let row = i32::try_from(Board::<Tile>::cell_position_to_index(
+                    thread_rng().gen_range(0..board.cell_width),
+                ))
+                .ok()?;
+                let col = 1;
+                Some(Perimeter {
+                    pair: Pair { row, col },
+                })
+            }
+            Direction::Up => {
+                let row = 1;
+                let col = i32::try_from(Board::<Tile>::cell_position_to_index(
+                    thread_rng().gen_range(0..board.cell_width),
+                ))
+                .ok()?;
+                Some(Perimeter {
+                    pair: Pair { row, col },
+                })
+            }
+            Direction::Right => {
+                let row = i32::try_from(board.grid.len().sub(2)).ok()?;
+                let col = i32::try_from(Board::<Tile>::cell_position_to_index(
+                    thread_rng().gen_range(0..board.cell_width),
+                ))
+                .ok()?;
+                Some(Perimeter {
+                    pair: Pair { row, col },
+                })
+            }
+        }
+    }
+
+    /// Choosese a random unvisited direction.
+    ///
+    /// ### Parameters
+    /// * `pair`: A `Pair` to access a `Board` with.
+    /// * `visited`: The `Board` of visitation status.
+    ///
+    /// ### Returns
+    /// * An optional direction.
+    #[inline]
+    #[must_use]
+    pub fn choose_random_unvisited_direction(
+        pair: Pair,
+        visited: &Board<VisitStatus>,
+    ) -> Option<Direction> {
+        let direction_choices = Self::get_unvisited_directions(pair, visited);
+        direction_choices
+            .into_iter()
+            .collect::<Vec<Direction>>()
+            .choose(&mut thread_rng())
+            .copied()
+    }
+
+    /// Gets unvisited directions.
+    ///
+    /// ### Parameters
+    /// * `pair`: A `Pair` to access a `Board` with.
+    /// * `visited`: The `Board` of visitation status.
+    ///
+    /// ### Returns
+    /// * A set of `Direction` enums.
+    #[inline]
+    #[must_use]
+    pub fn get_unvisited_directions(
+        pair: Pair,
+        visited: &Board<VisitStatus>,
+    ) -> HashSet<Direction> {
+        Direction::iter()
+            .filter(|direction| {
+                let Some(visit_status_of_new_pair) = visited
+                    .get_from_pair(pair.add(2_i32.mul(Pair::from(*direction))))
+                else {
+                    return false;
+                };
+                *visit_status_of_new_pair == VisitStatus::Unvisited
+            })
+            .collect::<HashSet<Direction>>()
+    }
 }
 
 #[cfg(test)]
@@ -261,18 +261,16 @@ mod test_maze {
     use strum::IntoEnumIterator;
 
     use crate::{
-        board::Board, direction::Direction, pair::Pair,
+        board::Board, direction::Direction, maze::Maze, pair::Pair,
         visit_status::VisitStatus,
     };
-
-    use super::get_unvisited_directions;
 
     #[test]
     fn test_get_univisited_directions() {
         let pair = Pair::from_row_and_col(3, 3);
         let mut board = Board::<VisitStatus>::new(6, 6);
         let none_visited = Direction::iter().collect::<HashSet<Direction>>();
-        assert_eq!(get_unvisited_directions(pair, &board), none_visited);
+        assert_eq!(Maze::get_unvisited_directions(pair, &board), none_visited);
 
         if let Some(cell) = board
             .get_mut_from_pair(pair + 2_i32.mul(Pair::from(Direction::Left)))
@@ -281,7 +279,7 @@ mod test_maze {
         }
         let mut left_visited = none_visited;
         let _: bool = left_visited.remove(&Direction::Left);
-        assert_eq!(get_unvisited_directions(pair, &board), left_visited);
+        assert_eq!(Maze::get_unvisited_directions(pair, &board), left_visited);
 
         if let Some(cell) = board
             .get_mut_from_pair(pair + 2_i32.mul(Pair::from(Direction::Right)))
@@ -291,7 +289,7 @@ mod test_maze {
         let mut left_and_right_visited = left_visited;
         let _: bool = left_and_right_visited.remove(&Direction::Right);
         assert_eq!(
-            get_unvisited_directions(pair, &board),
+            Maze::get_unvisited_directions(pair, &board),
             left_and_right_visited
         );
     }
